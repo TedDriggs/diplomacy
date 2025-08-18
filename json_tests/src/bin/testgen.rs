b1e8cef3c686b506f3afa14a3b21a291104e30a6
Ted Driggs <ted.driggs@outlook.com>
2025-08-18 08:04:36 -0700

    Don't add @generated as it doesn't work

--- json_tests/src/bin/testgen.rs
+++ json_tests/src/bin/testgen.rs
@@ -11,10 +11,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let cases = serde_json::from_str::<Cases<RawTestCase>>(&json)
         .map_err(|e| anyhow::Error::new(e).context(format!("Parsing {path}")))?;
 
-    println!(
-        "// @generated\n{}",
-        prettyplease::unparse(&syn::parse_quote!(#cases))
-    );
+    println!("{}", prettyplease::unparse(&syn::parse_quote!(#cases)));
 
     Ok(())
 }
