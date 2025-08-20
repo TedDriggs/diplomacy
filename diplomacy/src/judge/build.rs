7ba9dea71549a7d9e33154ff6361d6d66fc89f4f
Ted Driggs <ted.driggs@outlook.com>
2025-08-20 08:50:02 -0700

    Move standard rulebook build adjudication to rulebook module

--- diplomacy/src/judge/build.rs
+++ diplomacy/src/judge/build.rs
@@ -6,7 +6,6 @@ use crate::order::BuildCommand;
 use crate::{Nation, ShortName, Unit, UnitPosition, UnitType};
 use std::borrow::Cow;
 use std::collections::{HashMap, HashSet};
-use std::convert::TryInto;
 
 /// The outcome of a build-turn order.
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
@@ -166,10 +165,16 @@ impl<'a, W: WorldState, A: Adjudicate<'a>> Context<'a, W, A> {
     }
 }
 
+/// Mutable state for adjudicating a build phase.
+///
+/// This is created when calling [`Submission::adjudicate`] and passed to the [adjudicator](`Adjudicate`).
 pub struct ResolverState<'a, Scratch> {
+    /// Scratch state for the adjudicator. See [`Adjudicate::Scratch`] for more information.
     pub scratch: Scratch,
     state: HashMap<&'a MappedBuildOrder, OrderOutcome>,
+    /// The set of units that were disbanded due to civil disorder.
     pub civil_disorder: HashSet<UnitPosition<'a, RegionKey>>,
+    /// The final units after adjudication, grouped by nation.
     pub final_units: HashMap<&'a Nation, HashSet<(UnitType, RegionKey)>>,
 }
 
@@ -221,6 +226,7 @@ impl<'a, S> ResolverState<'a, S> {
     }
 }
 
+/// The outcome of a build-phase adjudication.
 #[derive(Debug, Clone)]
 pub struct Outcome<'a> {
     orders: HashMap<&'a MappedBuildOrder, OrderOutcome>,
@@ -229,6 +235,7 @@ pub struct Outcome<'a> {
 }
 
 impl<'a> Outcome<'a> {
+    /// Returns the outcome of an order.
     pub fn get(&self, order: &MappedBuildOrder) -> Option<&OrderOutcome> {
         self.orders.get(order)
     }
@@ -258,6 +265,7 @@ impl<'a> Outcome<'a> {
         })
     }
 
+    /// Returns the set of units that were disbanded due to civil disorder.
     pub fn to_civil_disorder(&self) -> HashSet<UnitPosition<'static, RegionKey>> {
         self.civil_disorder
             .iter()
@@ -271,9 +279,53 @@ impl<'a> Outcome<'a> {
     }
 }
 
+/// Adjudicator for a build-phase.
+///
+/// For implementation efficiency, this trait allows for a "scratch" state that will be stored
+/// in the resolver.
+pub trait Adjudicate<'a>: Sized {
+    /// Working storage for this adjudicator. This is used to store initial calculations
+    /// and mutable state that is needed to adjudicate orders that are not independent.
+    type Scratch;
+
+    /// Initialize the scratch state for this adjudicator. This is called once per resolution.
+    fn initialize<W: WorldState>(&self, ctx: &Context<'a, W, Self>) -> Self::Scratch;
+
+    /// Determines and returns the success of an order.
+    ///
+    /// Calling this MUST leave the `resolver` in the same state as if [`Adjudicate::explain`] was called.
+    ///
+    /// Adjudicators MAY customize this method to be more memory-efficient by avoiding allocation of explanatory data.
+    fn adjudicate<W: WorldState>(
+        &self,
+        context: &Context<'a, W, Self>,
+        resolver: &mut ResolverState<'a, Self::Scratch>,
+        order: &'a MappedBuildOrder,
+    ) -> OrderState {
+        self.explain(context, resolver, order).into()
+    }
+
+    /// Determines and returns the outcome of an order.
+    ///
+    /// The outcome contains enough information to determine both _whether_ the order succeeds or fails and _why_ the order succeeds or fails.
+    fn explain<W: WorldState>(
+        &self,
+        context: &Context<'a, W, Self>,
+        resolver: &mut ResolverState<'a, Self::Scratch>,
+        order: &'a MappedBuildOrder,
+    ) -> OrderOutcome;
+
+    /// Modifies the resolver after all orders have been adjudicated to enforce civil disorder rules.
+    fn civil_disorder<W: WorldState>(
+        &self,
+        context: &Context<'a, W, Self>,
+        resolver: &mut ResolverState<'a, Self::Scratch>,
+    );
+}
+
 /// Rulebook function for build-phase adjudication. This function does not worry about order quantities,
 /// and just focuses on whether or not a given build or disband command is otherwise valid.
-fn adjudicate<A>(
+pub(crate) fn adjudicate<A>(
     context: &Context<impl WorldState, A>,
     home_scs: &HashMap<&Nation, HashSet<ProvinceKey>>,
     order: &MappedBuildOrder,
@@ -329,268 +381,6 @@ pub fn to_initial_ownerships(map: &Map) -> HashMap<ProvinceKey, Nation> {
             }
         })
         .collect()
-}
-
-/// Adjudicator for a build-phase.
-///
-/// For implementation efficiency, this trait allows for a "scratch" state that will be stored
-/// in the resolver.
-pub trait Adjudicate<'a>: Sized {
-    /// Working storage for this adjudicator. This is used to store initial calculations
-    /// and mutable state that is needed to adjudicate orders that are not independent.
-    type Scratch;
-
-    /// Initialize the scratch state for this adjudicator. This is called once per resolution.
-    fn initialize<W: WorldState>(&self, ctx: &Context<'a, W, Self>) -> Self::Scratch;
-
-    /// Determines and returns the success of an order.
-    ///
-    /// Calling this MUST leave the `resolver` in the same state as if [`Adjudicate::explain`] was called.
-    ///
-    /// Adjudicators MAY customize this method to be more memory-efficient by avoiding allocation of explanatory data.
-    fn adjudicate<W: WorldState>(
-        &self,
-        context: &Context<'a, W, Self>,
-        resolver: &mut ResolverState<'a, Self::Scratch>,
-        order: &'a MappedBuildOrder,
-    ) -> OrderState {
-        self.explain(context, resolver, order).into()
-    }
-
-    /// Determines and returns the outcome of an order.
-    ///
-    /// The outcome contains enough information to determine both _whether_ the order succeeds or fails and _why_ the order succeeds or fails.
-    fn explain<W: WorldState>(
-        &self,
-        context: &Context<'a, W, Self>,
-        resolver: &mut ResolverState<'a, Self::Scratch>,
-        order: &'a MappedBuildOrder,
-    ) -> OrderOutcome;
-
-    /// Modifies the resolver after all orders have been adjudicated to enforce civil disorder rules.
-    fn civil_disorder<W: WorldState>(
-        &self,
-        context: &Context<'a, W, Self>,
-        resolver: &mut ResolverState<'a, Self::Scratch>,
-    );
-}
-
-/// Scratch space for standard variant build-phase adjudication.
-pub struct RuleScratch<'a> {
-    deltas: HashMap<&'a Nation, (BuildCommand, i16)>,
-    home_scs: HashMap<&'a Nation, HashSet<ProvinceKey>>,
-    ownerships: HashMap<&'a Nation, HashSet<ProvinceKey>>,
-}
-
-impl<'a> RuleScratch<'a> {
-    fn new<W: WorldState, A>(context: &Context<'a, W, A>) -> Self {
-        let mut home_scs = HashMap::<&Nation, HashSet<ProvinceKey>>::new();
-        let mut ownerships = HashMap::<&Nation, HashSet<ProvinceKey>>::new();
-
-        // Figure out who owns what and where nations are allowed to build.
-        for province in context
-            .world_map
-            .provinces()
-            .filter(|p| p.is_supply_center())
-        {
-            if let SupplyCenter::Home(nat) = &province.supply_center {
-                home_scs.entry(nat).or_default().insert(province.into());
-            }
-
-            let key = ProvinceKey::from(province);
-            if let Some(nation) = context
-                .this_time
-                .occupier(&key)
-                .or_else(|| context.last_time.get(&key))
-            {
-                ownerships.entry(nation).or_default().insert(key);
-            }
-        }
-
-        Self {
-            deltas: ownerships
-                .iter()
-                .filter_map(|(&nation, ownerships)| {
-                    let adjustment = i16::from(ownerships.len() as u8)
-                        - i16::from(context.this_time.unit_count(nation));
-                    match adjustment {
-                        0 => None,
-                        x if x > 0 => Some((nation, (BuildCommand::Build, x))),
-                        x => Some((nation, (BuildCommand::Disband, -x))),
-                    }
-                })
-                .collect(),
-            home_scs,
-            ownerships,
-        }
-    }
-}
-
-impl<'a> Adjudicate<'a> for crate::judge::Rulebook {
-    type Scratch = RuleScratch<'a>;
-
-    fn initialize<W: WorldState>(&self, context: &Context<'a, W, Self>) -> Self::Scratch {
-        Self::Scratch::new(context)
-    }
-
-    fn explain<W: WorldState>(
-        &self,
-        context: &Context<'a, W, Self>,
-        resolver: &mut ResolverState<'a, Self::Scratch>,
-        order: &'a MappedBuildOrder,
-    ) -> OrderOutcome {
-        use self::OrderOutcome::*;
-
-        let Some(delta) = resolver.scratch.deltas.get_mut(&order.nation) else {
-            return RedeploymentProhibited;
-        };
-
-        // A power is only allowed to build or disband in a given turn, not both
-        if delta.0 != order.command {
-            return RedeploymentProhibited;
-        }
-
-        let adjudication = adjudicate(context, &resolver.scratch.home_scs, order);
-
-        if adjudication != OrderOutcome::Succeeds {
-            return adjudication;
-        }
-
-        match order.command {
-            BuildCommand::Build => {
-                if delta.1 == 0 {
-                    return AllBuildsUsed;
-                }
-
-                delta.1 -= 1;
-
-                resolver
-                    .final_units
-                    .entry(&order.nation)
-                    .or_default()
-                    .insert((order.unit_type, order.region.clone()));
-
-                Succeeds
-            }
-            BuildCommand::Disband => {
-                if delta.1 == 0 {
-                    return AllDisbandsUsed;
-                }
-
-                delta.1 -= 1;
-
-                resolver
-                    .final_units
-                    .entry(&order.nation)
-                    .or_default()
-                    .remove(&(order.unit_type, order.region.clone()));
-
-                Succeeds
-            }
-        }
-    }
-
-    fn civil_disorder<W: WorldState>(
-        &self,
-        context: &Context<'a, W, Self>,
-        resolver: &mut ResolverState<'a, Self::Scratch>,
-    ) {
-        let world_graph = context.world_map.to_graph();
-
-        for (nation, delta) in &mut resolver.scratch.deltas {
-            if delta.0 == BuildCommand::Build || delta.1 == 0 {
-                continue;
-            }
-
-            let usize_delta: usize = delta.1.try_into().unwrap();
-            let units = resolver.final_units.remove(*nation).unwrap();
-
-            // Per 2023 rulebook, units disband based on distance from the nation's owned
-            // supply centers (earlier editions had it based on distance from the home supply centers)
-            let Some(owned_scs) = resolver.scratch.ownerships.get(*nation) else {
-                // If there are no owned supply centers, all units disband
-                resolver
-                    .civil_disorder
-                    .extend(units.into_iter().map(|unit| {
-                        UnitPosition::new(Unit::new(Cow::Borrowed(*nation), unit.0), unit.1)
-                    }));
-                continue;
-            };
-
-            // Get all regions in the owned supply centers. The rules require checking
-            // distance to all coasts, so province precision is insufficient.
-            let owned_sc_regions = context
-                .world_map
-                .regions()
-                .filter(|r| owned_scs.contains(r.province()))
-                .collect::<Vec<_>>();
-
-            let mut units_by_disband_priority = units
-                .into_iter()
-                .map(|unit| {
-                    let unit_region = context
-                        .world_map
-                        .find_region(&unit.1.to_string())
-                        .unwrap_or_else(|| {
-                            panic!("Unit location {} should exist in world", unit.1)
-                        });
-
-                    if owned_sc_regions.contains(&unit_region) {
-                        return (unit, 0);
-                    }
-
-                    let min_distance = owned_sc_regions
-                        .iter()
-                        .filter_map(|sc_region| {
-                            // Using dijkstra because there isn't an obvious way to estimate
-                            // distance for A*, and the graph size is so small that the efficiency
-                            // difference shouldn't matter.
-                            petgraph::algo::dijkstra(
-                                &world_graph,
-                                unit_region,
-                                Some(sc_region),
-                                // Per DATC test 6.J.6, terrain is ingored in this
-                                // calculation. This is a deviation from older versions
-                                // of the DATC, which stated that sea units could only
-                                // consider sea distances
-                                |_| 1,
-                            )
-                            .get(sc_region)
-                            .copied()
-                        })
-                        .min()
-                        .unwrap_or(i32::MAX);
-
-                    (unit, min_distance)
-                })
-                .collect::<Vec<_>>();
-
-            // Per the DATC, units are sorted by distance from an owned SC. Equidistant fleets
-            // are disbanded before armies, and sorting of units within the same type is done
-            // alphabetically.
-            units_by_disband_priority.sort_by(|a, b| {
-                // Distance from nearest owned supply center, descending
-                b.1.cmp(&a.1)
-                    // when equidistant, disband fleets before armies
-                    .then(b.0 .0.cmp(&a.0 .0))
-                    // when units are same type and equidistant, disband in alphabetical order
-                    .then_with(|| a.0 .1.cmp(&b.0 .1))
-            });
-
-            // Add units from the disband queue to the civil disorder output
-            resolver.civil_disorder.extend(
-                units_by_disband_priority
-                    .drain(0..usize_delta)
-                    .map(|v| UnitPosition::new(Unit::new(Cow::Borrowed(*nation), v.0 .0), v.0 .1)),
-            );
-
-            // Add the remaining units to the map of units that survive the turn.
-            resolver.final_units.insert(
-                nation,
-                units_by_disband_priority.into_iter().map(|v| v.0).collect(),
-            );
-        }
-    }
 }
 
 #[cfg(test)]
